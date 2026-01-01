110e445ae50bfaa1fdc2acd2ad64248a4272ee70
藤村暢寿 <noppy7313.gmail.com>
2026-01-02 00:38:32 +0900

    2025-12-31

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,12 +1,26 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+//クラスからエンティティへと変換、データベースへ値が保存される
+@Entity(name = "categories")
 public class Category {
+    //categoryIdをプライマリーキーとしてデータベース側で自動採番
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
     public Category(String categoryName, Long categoryId) {
         this.categoryName = categoryName;
         this.categoryId = categoryId;
+    }
+
+    //JPA使用時にデフォルトコンストラクタ要求を満たすためのデフォルトコンストラクター
+    public Category() {
     }
 
     public String getCategoryName() {
--- src/main/java/com/ecommerce/project/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.service.impl;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repository.CategoryRepository;
 import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,33 +16,38 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
+
+    //CategoryRepositoryのBeanを自動注入
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     //現在のすべてのカテゴリーを表示
     @Override
     public List<Category> getALLCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     //カテゴリー追加
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     //カテゴリーの更新
     @Override
     public Category updateCategory(Category category, Long categoryId) {
+        //categoryIdに一致するカテゴリーを見つける
         Optional<Category> category_updated = categories.stream()
                                                        .filter(c -> c.getCategoryId().equals(categoryId))
                                                        .findFirst();
 
+        //一致するカテゴリーを見つけたらexisitingCategory変数を仲介して値を更新する
+        //見つからなければ例外を返す
         if (category_updated.isPresent()) {
             Category exisitingCategory = category_updated.get();
             exisitingCategory.setCategoryName(category.getCategoryName());
 
-            return exisitingCategory;
+            return categoryRepository.save(exisitingCategory);
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,
                     "category not found");
@@ -50,12 +57,18 @@ public class CategoryServiceImpl implements CategoryService {
     //カテゴリー削除
     @Override
     public String deleteCategory(Long categoryId) {
+
+        //categoryIdに一致するカテゴリーを見つける、見つからなければ例外を返す
         Category category_deleted = categories.stream()
                                       .filter(c -> c.getCategoryId().equals(categoryId))
                                       .findFirst()
                                       .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                                               "category not found"));
-        categories.remove(category_deleted);
+
+        //見つかれば削除する
+        categoryRepository.delete(category_deleted);
+
+        //削除成功メッセージを返す
         return "category with categoryId:" + categoryId + " deleted successfully";
     }
 }
